01ef0f2cb2ba4eb9bee8f7ee5c8d3196fb6090c0
untukgrowtopia9-afk <untukgrowtopia9@gmail.com>
2026-03-03 17:15:01 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -61,7 +61,7 @@ _G.FishItConfig = _G.FishItConfig or {
             "Blob Shark",
             "Ghost Shark",
         },
-        ["FARM_LOC_SECRET_SACRIFICE"] = "Treasure Room",
+        ["FARM_LOC_SECRET_SACRIFICE"] = "Tropical Grove",
 
         ["Minimum Rod"] = "Astral Rod",
     },
