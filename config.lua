8e9700e9c09bf1edce5f48b038e73a274065a620
untukgrowtopia9-afk <untukgrowtopia9@gmail.com>
2026-03-12 04:45:46 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -37,7 +37,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Selling"] = {
         ["Auto Sell"] = true,
-        ["Auto Sell Threshold"] = "Legendary",
+        ["Auto Sell Threshold"] = "Mythic",
         ["Auto Sell Every"] = 50,
     },
     ["Doing Quest"] = {
