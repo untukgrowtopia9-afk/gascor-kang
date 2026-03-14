79d92abaed10e9e67352756f75863e75eab80750
untukgrowtopia9-afk <untukgrowtopia9@gmail.com>
2026-03-14 13:20:37 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -109,7 +109,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Roll Enchant"] = true,
         ["Evolved Roll Enchant"] = false,
         ["Enchant List"] = {
-            "Cursed I",
+            "Reeler I",
         },
         ["Second Enchant"] = false,
         ["Allowed Sacrifice"] = {
